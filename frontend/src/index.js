e342342ac682a06064305b26009c8ccd117ee778
David Blot <david.blot9@gmail.com>
2022-10-04 17:00:46 +0200

    add Router in index.js + del App.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+// import de composants depuis la bibliothèque React pour créer la navigation :
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import des pages pour le routeur :
+import Home from './pages/Home';
+import About from './pages/About';
+import Error404 from './pages/Error404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
-	
 );
