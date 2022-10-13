4d22ebbafd74478307645e8d3cab64c8a30f9e77
David Blot <david.blot9@gmail.com>
2022-10-13 18:16:07 +0200

    fix Card style

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = ({ cover, title }) => {
 	return (
 		<div className="lodging-card">
-			<h3 className="lodging-title">{title}</h3>
 			<img src={cover} alt={title} />
+			<h3 className="lodging-title">{title}</h3>
 		</div>
 	);
 };
